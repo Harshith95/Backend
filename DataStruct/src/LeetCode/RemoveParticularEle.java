6ff9e64b9609ca114dc016f3a605180f26515920
Harshith <kathakuli05@gmail.com>
2025-03-16 12:01:59 +0530

    Leet Code solved
    Leet Code solved

--- DataStruct/src/LeetCode/RemoveParticularEle.java
+++ DataStruct/src/LeetCode/RemoveParticularEle.java
@@ -5,22 +5,28 @@ public class RemoveParticularEle {
 	public static void main(String[] args) {
 		int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
 		System.out.println("Length: " + nums.length);
-		removeElement(nums, 0);
+		int res = removeElement(nums, 0);
 		System.out.println("Length: AFTER" + nums.length);
 		for (int i = 0; i < nums.length; i++) {
 			System.out.print(nums[i] + " ");
 		}
 	}
 
+	/*
+	 * public static int removeElement(int[] nums, int val) { int j = 0; for (int i
+	 * = 0; i < nums.length; i++) { if (nums[i] != val) { nums[j] = nums[i]; j++; }
+	 * } return j; }
+	 */
+
 	public static int removeElement(int[] nums, int val) {
-		int j = 0;
+
 		for (int i = 0; i < nums.length; i++) {
-			if (nums[i] != val) {
-				nums[j] = nums[i];
-				j++;
+			if (nums[i] == val) {
+				nums[i] = nums[++i];
 			}
 		}
-		return j;
+		System.out.println("Res"+ nums.length);
+		return nums.length;
 	}
 
 }
