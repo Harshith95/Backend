93630cbf862f3e6bc5e9585860c14e2ab30707fe
Harshith <kathakuli05@gmail.com>
2025-07-09 11:21:51 +0530

    Add 2 large number string
    Datastructire which adds 2 large string numbers and returns result

--- DataStruct/src/LeetCode/AddStrings.java
+++ DataStruct/src/LeetCode/AddStrings.java
@@ -1,18 +1,30 @@
 package LeetCode;
 
+import java.math.BigInteger;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class AddStrings {
 
-	public static void main(String[] args) {
-		System.out.println(addStrings("1", "1"));
-	}
+    public static void main(String[] args) {
+        System.out.println(addStrings("1,234,567,890,123,456", "987,654,321,876,544"));
+    }
 
-	public static String addStrings(String num1, String num2) {
-		double nm1 = Double.valueOf(num1);
-		double nm2 = Integer.valueOf(num2);
-		System.out.println(nm1);
-		System.out.println(nm2);
-		double res = nm1 + nm2;
-		return String.valueOf(res);
-	}
+    /**
+     * Using Library approach
+     *
+     * @param num1
+     * @param num2
+     * @return
+     */
+    public static String addStrings(String num1, String num2) {
+        BigInteger nm1 = new BigInteger(num1.replaceAll(",", ""));
+        BigInteger nm2 = new BigInteger(num2.replaceAll(",", ""));
+        System.out.println(nm1);
+        System.out.println(nm2);
+        BigInteger res = nm1.add(nm2);
+        String ans = NumberFormat.getInstance(Locale.US).format(res);
+        return ans;
+    }
 
 }
