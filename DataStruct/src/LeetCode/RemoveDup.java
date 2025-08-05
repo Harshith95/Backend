fb5f1992032d871e291a11f4cf009910610ee5f8
Harshith <kathakuli05@gmail.com>
2025-08-05 10:25:03 +0530

    push DS
    push DS

--- DataStruct/src/LeetCode/RemoveDup.java
+++ DataStruct/src/LeetCode/RemoveDup.java
@@ -1,90 +1,93 @@
 package LeetCode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
 /**
  * Given an integer array nums sorted in non-decreasing order, remove the
  * duplicates in-place such that each unique element appears only once. The
  * relative order of the elements should be kept the same. Then return the
  * number of unique elements in nums.
- * 
+ * <p>
  * Consider the number of unique elements of nums to be k, to get accepted, you
  * need to do the following things:
- * 
+ * <p>
  * Change the array nums such that the first k elements of nums contain the
  * unique elements in the order they were present in nums initially. The
  * remaining elements of nums are not important as well as the size of nums.
  * Return k.
- *
- *
+ * <p>
+ * <p>
  * Custom Judge:
- * 
+ * <p>
  * The judge will test your solution with the following code:
- * 
+ * <p>
  * int[] nums = [...]; // Input array
- * 
+ * <p>
  * int[] expectedNums = [...]; // The expected answer with correct length
- * 
+ * <p>
  * int k = removeDuplicates(nums); // Calls your implementation
- * 
+ * <p>
  * assert k == expectedNums.length;
- * 
+ * <p>
  * for (int i = 0; i < k; i++) { assert nums[i] == expectedNums[i]; }
- * 
+ * <p>
  * If all assertions pass, then your solution will be accepted.
- * 
- * 
+ * <p>
+ * <p>
  * Example 1:
- * 
+ * <p>
  * Input: nums = [1,1,2] Output: 2, nums = [1,2,_] Explanation: Your function
  * should return k = 2, with the first two elements of nums being 1 and 2
  * respectively. It does not matter what you leave beyond the returned k (hence
  * they are underscores).
- * 
- * 
+ * <p>
+ * <p>
  * Example 2:
- * 
+ * <p>
  * Input: nums = [0,0,1,1,1,2,2,3,3,4] Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
  * Explanation: Your function should return k = 5, with the first five elements
  * of nums being 0, 1, 2, 3, and 4 respectively. It does not matter what you
  * leave beyond the returned k (hence they are underscores).
- * 
  */
 
 public class RemoveDup {
-	public static void main(String[] args) {
-		int[] nums = { 0, 0, 1, 1, 1, 2, 4, 3, 3, 4 };
+    public static void main(String[] args) {
+        int[] nums = {0, 0, 1, 1, 1, 2, 4, 3, 3, 4};
 
 
-		for (int i = 0; i < nums.length; i++) {
-			System.out.print(nums[i] + "  ");
-		}
-		System.out.println();
-		int k = removeDuplicates(nums);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + "  ");
+        }
+        System.out.println();
+        int k = removeDuplicates(nums);
 
-		for (int i = 0; i <= k; i++) {
-			System.out.print(nums[i] + "  ");
-		}
+        for (int i = 0; i <= k; i++) {
+            System.out.print(nums[i] + "  ");
+        }
 
-	}
+    }
 
-	public static int removeDuplicates(int[] nums) {
-		int rd = 0;
-		for (int i = 1; i < nums.length; i++) {
-			if (nums[rd] != nums[i]) {
-				System.out.println("Before increementing rd value " + rd + " i value " + i);
-				rd++;
-				System.out.println("After increementing rd value " + rd);
-				nums[rd] = nums[i];
-				System.out.println("After as nums value " + nums[rd]);
-			}
-		}
+    /**
+     * *
+     * * Input: nums = [0,0,1,1,1,2,2,3,3,4] Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+     * * Explanation: Your function should return k = 5, with the first five elements
+     * * of nums being 0, 1, 2, 3, and 4 respectively. It does not matter what you
+     * * leave beyond the returned k (hence they are underscores).
+     * *
+     *
+     * @param nums
+     * @return
+     */
+    public static int removeDuplicates(int[] nums) {
+        int rd = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[rd] != nums[i]) {
+                System.out.println("Before increementing rd value " + rd + " i value " + i);
+                rd++;
+                System.out.println("After increementing rd value " + rd);
+                nums[rd] = nums[i];
+                System.out.println("After as nums value " + nums[rd]);
+            }
+        }
 
-		return rd;
-	}
+        return rd;
+    }
 }
