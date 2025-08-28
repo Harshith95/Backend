0e0b51e7d7386ad591f49a2a5416b3b836772551
Harshith <kathakuli05@gmail.com>
2025-08-28 10:43:48 +0530

    UPdates in DSA
    UPdates in DSA

--- DataStruct/src/SlidingWindow/LongestUniqueSubstring.java
+++ DataStruct/src/SlidingWindow/LongestUniqueSubstring.java
@@ -4,7 +4,8 @@ import java.util.HashSet;
 
 /**
  * 🧩 Problem:
- * Given a string, find the length of the longest substring with all unique (non-repeating) characters.
+ * Given a string, find the length of the longest substring with all unique
+ * (non-repeating) characters.
  * <p>
  * ✨ Why Sliding Window?
  * You're looking at substrings (continuous characters).
@@ -21,20 +22,22 @@ import java.util.HashSet;
  */
 public class LongestUniqueSubstring {
     public static void main(String[] args) {
-        String s = "abcabcbb";
+        String s = "abcd";
 //        System.out.println("Length: " + lengthOfLongestSubstring(s));
         HashSet<Character> seen = new HashSet<>();
         int left = 0, right = 0, maxLength = 0;
         while (right < s.length()) {
             if (!seen.contains(s.charAt(right))) {
-                System.out.println("Value" + s.charAt(right));
+                System.out.println("Value If:: " + s.charAt(right));
                 seen.add(s.charAt(right));
                 maxLength = Math.max(maxLength, right - left + 1);
                 right++;
             } else {
+                System.out.println("Value Else:: " + s.charAt(right));
                 seen.remove(s.charAt(left)); // Shrink window
                 left++;
             }
+            System.out.println("Indexes:: " + left + " ::: " + right);
         }
         System.out.println("Max Length :: " + maxLength);
     }
